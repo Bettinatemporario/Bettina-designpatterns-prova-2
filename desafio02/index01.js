c5d5bbe0858c38053b68be6b21a602485ceadf9e
Bettinatemporario <pokemongeopets@gmail.com>
2025-10-30 19:50:29 -0300

    Update index01.js

--- desafio02/index01.js
+++ desafio02/index01.js
@@ -1,1 +1,53 @@
-console.log("Hello, World! 001");
+class PaymentProcessor {
+  processPayment(amount) {
+    console.log(`Pagamento de R$${amount} processado pelo sistema interno.`);
+  }
+}
+
+
+class LegacyPaymentSystem {
+  makePayment(amount) {
+    console.log(`Pagando R$${amount} com sistema legado.`);
+  }
+}
+
+
+class ModernPaymentAPI {
+  process(amountInCents) {
+    console.log(`Pagamento de R$${amountInCents / 100} via API moderna.`);
+  }
+}
+
+
+class ModernPaymentAdapter extends PaymentProcessor {
+  constructor(modernAPI) {
+    super();
+    this.modernAPI = modernAPI;
+  }
+
+  processPayment(amount) {
+  
+    const amountInCents = amount * 100;
+    this.modernAPI.process(amountInCents);
+  }
+}
+
+
+function payOrder(processor, amount) {
+  processor.processPayment(amount);
+}
+
+
+const internalProcessor = new PaymentProcessor();
+payOrder(internalProcessor, 100);
+
+
+const legacy = new LegacyPaymentSystem();
+legacy.processPayment = legacy.makePayment.bind(legacy);
+payOrder(legacy, 150);
+
+
+const modernAPI = new ModernPaymentAPI();
+const adaptedModern = new ModernPaymentAdapter(modernAPI);
+payOrder(adaptedModern, 200);
+
